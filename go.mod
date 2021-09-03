f53ced2c446f74364bb2a8e0d2c3c18f22a8443c
wuhui.zhang <ziegler@nd.com.cn>
2021-09-03 14:36:45 +0800

    use zap

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.17
 
 require (
 	github.com/apache/thrift v0.14.2
-	github.com/cihub/seelog v0.0.0-20170130134532-f561c5e57575
 	go.uber.org/zap v1.19.0
 )
 
--- thrift_service.go
+++ thrift_service.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"Bush/gen-go/user_service"
+	"Bush/log"
 	"context"
 	"fmt"
 
-	log "github.com/cihub/seelog"
 )
 
 type UserService struct{}
 
 func (this *UserService) GetUser(ctx context.Context, id int32) (*user_service.RcpResponse, error) {
-	log.Infof("get user by id:%d", id)
+	log.InfoNt(log.Message("get user by id:%d", id))
 	return &user_service.RcpResponse{
 		UserInfo: &user_service.UserInfo{
 			ID: id, Name: fmt.Sprintf("user:%d", id),
